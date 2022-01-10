3b19b76a53fb3cad33cf5196dc56b5d3d22cec03
suzunone <suzunone.eleven@gmail.com>
2022-01-10 17:49:31 +0900

    init vue

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { VueFormReplaySample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  VueFormReplaySample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-form-replay-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as VueFormReplaySample } from './vue-form-replay-sample.vue';
